58818ca36f85942728e8b63f7e47c0116c19bde6
connor-r78 <connorrakov@gmail.com>
2025-11-20 00:59:07 +0000

    calcToken() converts token to unique ID

--- src/bitchat/bitchat.c
+++ src/bitchat/bitchat.c
@@ -7,11 +7,27 @@ extern long _input();
 
 extern char input[0x1000];
 
+int calcToken( char* token, int tokenOffset )
+{
+  tokenOffset *= 4;
+
+  int tmp = 0;
+
+  for ( int i = tokenOffset; i < 4 + tokenOffset; i++ ) {
+    char letter = token[i];
+    letter -= 0x41;
+    if ( letter < 0 ) letter = 0;
+    tmp += letter + 26 * i;
+  }
+
+  return tmp;
+}
+
 int main()
 {
   _input();
 
-  printf("%s", input);
+  calcToken(input, 0);
 
   FILE *tokens;
   FILE *hiddenLayer;
